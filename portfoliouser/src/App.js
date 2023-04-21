3cc4a9528e0387c64c07023b6620a2f20825347e
Varidh Srivastav <varidhsrivastava19145@gmail.com>
2023-04-21 15:36:54 +0530

    updated

--- portfoliouser/src/App.js
+++ portfoliouser/src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './App.css';
-import Footer from './pages/GlobalPages/Footer/Footer';
-import Navbar from './pages/GlobalPages/Navbar/Navbar';
-import Landingpage from './pages/homepages/Landingpage';
+import React from "react";
+import "./App.css";
+import Footer from "./pages/GlobalPages/Footer/Footer";
+import Navbar from "./pages/GlobalPages/Navbar/Navbar";
+import Landingpage from "./pages/homepages/Landingpage";
 
 function App() {
   return (
     <div className="App">
-    {/* <Navbar/> */}
-    <Landingpage/>
-    <Footer/>
+      <Landingpage />
+      <Footer />
     </div>
   );
 }
